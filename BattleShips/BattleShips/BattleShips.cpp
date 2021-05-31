3767628051639c4e16c3dffd07a890690539e10c
Amy S <enechan444@gmail.com>
2021-05-31 15:57:27 +0200

    BattleShip 00 (Taking Notes)

---
+++ BattleShips/BattleShips/BattleShips.cpp
@@ -0,0 +1,60 @@
+// BattleShips.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    /*
+    Make an AI file.
+    Make a base system file.
+        //Might need to make it into a Function File for the Grid
+        Need a Grid system:
+        -------------------
+            One for Player and One for AI.
+                string array[10] gridH {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
+                string array[10] gridV {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+            Prototype:
+            //Horizontal A-J
+                cout << "[" << gridH[0] << "]" << "[" << gridH[1] << "]" << "[" << gridH[2] << "]" 
+                     << "[" << gridH[3] << "]" << "[" << gridH[4] << "]" << "[" << gridH[5] << "]" 
+                     << "[" << gridH[6] << "]" << "[" << gridH[7] << "]" << "[" << gridH[8] << "]" 
+                     << "[" << gridH[9] << "]" << endl;
+		        
+            //Vertical 1-10
+                cout << "[" << gridV[0] << "]" << "[" << gridV[1] << "]" << "[" << gridV[2] << "]" 
+                     << "[" << gridV[3] << "]" << "[" << gridV[4] << "]" << "[" << gridV[5] << "]" 
+                     << "[" << gridV[6] << "]" << "[" << gridV[7] << "]" << "[" << gridV[8] << "]" 
+                     << "[" << gridV[9] << "]" << endl;
+        
+        Ship amount:
+        -------------------
+        //Might need to make a Ship Function File
+        Ship Class:     Size:
+        Carrier	    |   5
+	    Battleship	|   4
+	    Cruiser	    |   3
+	    Submarine	|   3
+	    Destroyer   |   2
+
+
+    Make "Kaboom" and "Splush" for hit and Miss (Windwaker for fun).
+    
+    */
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
